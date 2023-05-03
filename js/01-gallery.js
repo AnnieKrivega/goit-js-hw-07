63254179ebd91939983fe7ae9e89663f66d5c126
anniekrivega <121309759+AnnieKrivega@users.noreply.github.com>
2023-05-03 15:21:26 +0300

    add

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,39 +2,46 @@ import { galleryItems } from './gallery-items.js';
 const gallery = document.querySelector('.gallery');
 
 const createGalleryMarkup = items => {
-    return items.map(({ preview, original, description }) => {
-        return `
-        <li class="gallery__item">
+return items.map(({ preview, original, description }) => {
+    return `
+    <li class="gallery__item">
         <a class="gallery__link" href="${original}">
-        <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
+        <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+        />
         </a>
-        <li/>
-        `;
+    </li>
+    `;
     })
     .join('');
 };
 
 gallery.innerHTML += createGalleryMarkup(galleryItems);
 
-let lightbox;
+let lightbox; 
 
 const openImage = event => {
-    const { nodeName, dataset, alt } = event.target;
-    event.preventDefault();
-    if (nodeName !== 'IMG') {
-        return;
-    }
-    lightbox = basicLightbox.create(`
-    <img src="${dataset.source}" alt="${alt}" />`);
-    lightbox.show();
-    document.addEventListener('keydown', closeImage);
+const { nodeName, dataset, alt } = event.target;
+event.preventDefault();
+if (nodeName !== 'IMG') {
+    return;
+}
+lightbox = basicLightbox.create(`
+    <img src="${dataset.source}" alt="${alt}" />
+`);
+lightbox.show();
+document.addEventListener('keydown', closeImage);
 };
 
 gallery.addEventListener('click', openImage);
 
 const closeImage = event => {
-    if (event.key === 'Escape') {
-        lightbox.close();
-        document.removeEventListener('keydown', closeImage);
-    }
+if (event.key === 'Escape') {
+    lightbox.close();
+    document.removeEventListener('keydown', closeImage);
+}
 };
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,8 +2,8 @@ import { galleryItems } from './gallery-items.js';
 const gallery = document.querySelector('.gallery');
 
 const createGalleryMarkup = items => {
-    return items.map(({ preview, original, description }) => {
-        return  `
+return items.map(({ preview, original, description }) => {
+    return `
         <li class="gallery__item">
         <a class="gallery__link" href="${original}">
             <img class="gallery__image" src="${preview}" alt="${description}" />
@@ -17,19 +17,19 @@ const createGalleryMarkup = items => {
 gallery.innerHTML += createGalleryMarkup(galleryItems);
 
 const lightboxOptions = {
-    captions: true,
-    captionsDate: 'alt',
-    captionsDelay: 250,
+captions: true,
+captionsData: 'alt',
+captionDelay: 250,
 };
 
 const lightbox = new SimpleLightbox('.gallery a', lightboxOptions);
 
 const openImage = event => {
-    event.preventDefault();
-    lightbox.open({
-        source: event.target.parentNode.href,
-        caption: event.target.alt,
-    });
+event.preventDefault();
+lightbox.open({
+    source: event.target.parentNode.href,
+    caption: event.target.alt,
+});
 };
 
 gallery.addEventListener('click', openImage);
