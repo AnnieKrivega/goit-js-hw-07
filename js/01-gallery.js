8c5a717639db9e1f3469826e05312251ec564f70
anniekrivega <121309759+AnnieKrivega@users.noreply.github.com>
2023-05-03 15:14:00 +0300

    add

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,40 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+const gallery = document.querySelector('.gallery');
 
-console.log(galleryItems);
+const createGalleryMarkup = items => {
+    return items.map(({ preview, original, description }) => {
+        return `
+        <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
+        </a>
+        <li/>
+        `;
+    })
+    .join('');
+};
+
+gallery.innerHTML += createGalleryMarkup(galleryItems);
+
+let lightbox;
+
+const openImage = event => {
+    const { nodeName, dataset, alt } = event.target;
+    event.preventDefault();
+    if (nodeName !== 'IMG') {
+        return;
+    }
+    lightbox = basicLightbox.create(`
+    <img src="${dataset.source}" alt="${alt}" />`);
+    lightbox.show();
+    document.addEventListener('keydown', closeImage);
+};
+
+gallery.addEventListener('click', openImage);
+
+const closeImage = event => {
+    if (event.key === 'Escape') {
+        lightbox.close();
+        document.removeEventListener('keydown', closeImage);
+    }
+};
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,35 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+const gallery = document.querySelector('.gallery');
 
-console.log(galleryItems);
+const createGalleryMarkup = items => {
+    return items.map(({ preview, original, description }) => {
+        return  `
+        <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+        </li>
+    `;
+    })
+    .join('');
+};
+
+gallery.innerHTML += createGalleryMarkup(galleryItems);
+
+const lightboxOptions = {
+    captions: true,
+    captionsDate: 'alt',
+    captionsDelay: 250,
+};
+
+const lightbox = new SimpleLightbox('.gallery a', lightboxOptions);
+
+const openImage = event => {
+    event.preventDefault();
+    lightbox.open({
+        source: event.target.parentNode.href,
+        caption: event.target.alt,
+    });
+};
+
+gallery.addEventListener('click', openImage);
